625ae2568a0b06d6eaa6aa6b8343e361771b8cd2
IPrashamsaPrabhu <iprashamsaprabhu.brigade@gmail.com>
2024-03-15 21:22:15 +0530

    project

---
+++ fitnessfreak/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,28 @@
+"use client"
+import React from 'react'
+import logo from "@/assets/logo.png"
+import { IoIosBody } from 'react-icons/io'
+import './Navbar.css'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Navbar = () => {
+  const [isloggedin, setIsloggedin] = React.useState<boolean>(false)
+  return (
+    <nav>
+         <Image src={logo} alt="Logo" />
+         <Link href='/'>Home</Link>
+            <Link href='/about'>About</Link>
+            <Link href='/profile'><IoIosBody /></Link>
+            <button>Sign Out</button>
+            {
+              isloggedin ?
+              <button>Logout</button>
+              :
+              <button>Login</button>
+            }
+    </nav>
+  )
+}
+
+export default Navbar
