7a0e5e3f7c78cc45754e083ef9c969f55fe5a9c4
IPrashamsaPrabhu <iprashamsaprabhu.brigade@gmail.com>
2024-03-31 18:24:36 +0530

    project

---
+++ fitnessfreak_backend/Middlewares/errorMiddleware.js
@@ -0,0 +1,16 @@
+//if there is a problem with any of the api,automatically errorhandler middleware will be called
+function errorHandler(statusCode, err, req, res, next) {
+    console.error(err.stack);
+
+    if (res.headersSent) {
+        return next(err);
+    }
+    
+    console.log("ERROR MIDDLEWARE CALLED")
+    res.status(statusCode || 500).json({
+        ok: false, // Set the "ok" field to false for errors
+        message: err.message,
+    });
+}
+
+module.exports = errorHandler;
